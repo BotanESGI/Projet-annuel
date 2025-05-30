6c10a2975868f3dd28ed99730b4f8bc5bb69a5be
Botan YILMAZ <byilmaz1@myges.fr>
2025-05-30 20:56:20 +0200

    fix backend api

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,9 +1,11 @@
 <?php
-header("Content-Type: application/json");
+header('Content-Type: application/json');
 
-$products = [
-    ["id" => 1, "name" => "Produit 1", "price" => 100],
-    ["id" => 2, "name" => "Produit 2", "price" => 200],
-];
-
-echo json_encode($products);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo json_encode(['message' => 'Page d\'accueil']);
+}
+else
+{
+    http_response_code(405);
+    echo json_encode(['error' => 'Méthode non autorisée']);
+}
